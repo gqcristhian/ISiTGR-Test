ccbcb9519ade5ee13c74d7243b3a520b90f7e330
gqcristhian <gqcristhian@utdallas.edu>
2020-02-25 00:08:54 -0600

    again

--- camb/docs/source/conf.py
+++ camb/docs/source/conf.py
@@ -13,8 +13,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../isitgr'))
-print(os.path.abspath('../../isitgr'))
+sys.path.insert(0, os.path.abspath('..'))
+print(os.path.abspath('..'))
 print("...............................................")
 
 # -- Project information -----------------------------------------------------
